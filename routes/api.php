cfab8aed10070fd4792055bc0d652bf242ebec85
alvinn <110670962+slvinnm@users.noreply.github.com>
2026-09-13 06:20:02 +0700

    feat: expose reception queue registration api

--- routes/api.php
+++ routes/api.php
@@ -1,6 +1,7 @@
 <?php
 
 use App\Http\Controllers\Api\KioskController;
+use App\Http\Controllers\Api\ReceptionController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
@@ -10,7 +11,16 @@ Route::prefix('v1')->group(function () {
         Route::post('/queue-acquisitions', [KioskController::class, 'acquire']);
     });
 
-    Route::get('/user', function (Request $request) {
-        return $request->user();
-    })->middleware('auth:sanctum');
+    Route::middleware('auth:sanctum')->group(function () {
+        Route::prefix('reception')->group(function () {
+            Route::post(
+                '/queue-acquisitions/{queueAcquisition}/register',
+                [ReceptionController::class, 'registerQueueAcquisition']
+            );
+        });
+
+        Route::get('/user', function (Request $request) {
+            return $request->user();
+        });
+    });
 });
