d3e212fac8d98fa54614d91e2a45f5465214e0f7
alvinn <110670962+slvinnm@users.noreply.github.com>
2026-09-12 22:47:48 +0700

    feat: expose kiosk queue acquisition api

--- routes/api.php
+++ routes/api.php
@@ -1,8 +1,16 @@
 <?php
 
+use App\Http\Controllers\Api\KioskController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
-Route::get('/user', function (Request $request) {
-    return $request->user();
-})->middleware('auth:sanctum');
+Route::prefix('v1')->group(function () {
+    Route::prefix('kiosk')->group(function () {
+        Route::get('/departments', [KioskController::class, 'departments']);
+        Route::post('/queue-acquisitions', [KioskController::class, 'acquire']);
+    });
+
+    Route::get('/user', function (Request $request) {
+        return $request->user();
+    })->middleware('auth:sanctum');
+});
