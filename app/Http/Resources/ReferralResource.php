8996f10a01299302a35cb1216234c115ed08743f
alvinn <110670962+slvinnm@users.noreply.github.com>
2026-09-13 06:28:01 +0700

    feat: add referral resource

---
+++ app/Http/Resources/ReferralResource.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class ReferralResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'status' => $this->status?->value,
+            'reason' => $this->reason,
+            'source_visit' => [
+                'id' => $this->sourceVisit?->id,
+                'visit_number' => $this->sourceVisit?->visit_number,
+                'department' => [
+                    'id' => $this->sourceVisit?->department?->id,
+                    'code' => $this->sourceVisit?->department?->code,
+                    'name' => $this->sourceVisit?->department?->name,
+                ],
+            ],
+            'target_department' => [
+                'id' => $this->targetDepartment?->id,
+                'code' => $this->targetDepartment?->code,
+                'name' => $this->targetDepartment?->name,
+            ],
+            'target_visit' => [
+                'id' => $this->targetVisit?->id,
+                'visit_number' => $this->targetVisit?->visit_number,
+                'status' => $this->targetVisit?->status?->value,
+                'queue_tickets' => QueueTicketResource::collection(
+                    $this->whenLoaded('targetVisit')->targetVisit?->queueTickets ?? collect(),
+                ),
+            ],
+            'referred_by_user_id' => $this->referred_by_user_id,
+            'created_at' => $this->created_at?->toISOString(),
+        ];
+    }
+}
