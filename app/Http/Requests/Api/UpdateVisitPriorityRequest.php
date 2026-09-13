6c94f744b0c5a7d94da37d4fc5667660b02f9809
alvinn <110670962+slvinnm@users.noreply.github.com>
2026-09-13 07:28:51 +0700

    feat: add visit priority request

---
+++ app/Http/Requests/Api/UpdateVisitPriorityRequest.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\Http\Requests\Api;
+
+use App\Enums\Priority;
+use Illuminate\Foundation\Http\FormRequest;
+use Illuminate\Validation\Rule;
+
+class UpdateVisitPriorityRequest extends FormRequest
+{
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    public function rules(): array
+    {
+        return [
+            'priority' => [
+                'required',
+                'integer',
+                Rule::in(array_map(
+                    static fn (Priority $priority): int => $priority->value,
+                    Priority::cases(),
+                )),
+            ],
+        ];
+    }
+}
