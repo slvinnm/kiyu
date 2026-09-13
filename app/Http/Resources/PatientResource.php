d39d9582116499e74938ca8b97dfe57c2b1124c6
alvinn <110670962+slvinnm@users.noreply.github.com>
2026-09-13 07:08:19 +0700

    feat: add patient api resource

---
+++ app/Http/Resources/PatientResource.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class PatientResource extends JsonResource
+{
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'medical_record_number' => $this->medical_record_number,
+            'name' => $this->name,
+            'date_of_birth' => $this->date_of_birth?->toDateString(),
+            'gender' => $this->gender,
+            'phone' => $this->phone,
+            'email' => $this->email,
+            'address' => $this->address,
+        ];
+    }
+}
