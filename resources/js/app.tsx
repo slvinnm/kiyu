608f54f7f43cb740af982127dd7693f300cf4344
Alvin Nuha <alvinn549@gmail.com>
2026-09-14 09:12:49 +0700

    setup shadcn theme

--- resources/js/app.tsx
+++ resources/js/app.tsx
@@ -1,9 +1,17 @@
 import { createInertiaApp } from '@inertiajs/react';
+import { ThemeProvider } from "@/components/theme-provider"
 
 const appName = import.meta.env.VITE_APP_NAME || 'Laravel';
 
 void createInertiaApp({
     title: (title) => (title ? `${title} - ${appName}` : appName),
+    withApp(app) {
+        return (
+            <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+                {app}
+            </ThemeProvider>
+        );
+    },
     progress: {
         color: '#4B5563',
     },
