b1d44e073960e1b061bc83ebecd4cedec6357a98
alvinn <110670962+slvinnm@users.noreply.github.com>
2026-09-22 18:40:58 +0700

    restore valid queue acquisition factory

---
+++ database/factories/QueueAcquisitionFactory.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace DatabaseFactories;
+
+use App\Models\QueueAcquisition;
+use App\Models\Visit;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class QueueAcquisitionFactory extends Factory
+{
+    protected $model = QueueAcquisition::class;
+
+    public function definition(): array
+    {
+        return [
+            'department_id' => 1,
+            'visit_id' => Visit::factory(),
+            'channel' => 'KIOSK',
+            'idempotency_key' => null,
+            'status' => 'ACQUIRED',
+            'registered_by' => null,
+            'acquired_at' => now(),
+            'registered_at' => null,
+        ];
+    }
+}
